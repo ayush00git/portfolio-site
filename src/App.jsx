090a4f7829267898b6d58cf2eebcdc23af6db4d9
ayush00git <ayush.2007.iit@gmail.com>
2026-01-26 20:08:28 +0530

    dez: added lenis scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import { useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Lenis from 'lenis'
 import HomePage from './Pages/HomePage'
 import DockComp from './components/DockComp'
 import ProjectPage from './Pages/ProjectPage'
@@ -7,6 +9,21 @@ import Achievements from './Pages/AchievementPage'
 import YapsPage from './Pages/YapsPage'
 
 function AppContent() {
+  useEffect(() => {
+    const lenis = new Lenis()
+
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+
+    return () => {
+      lenis.destroy()
+    }
+  }, [])
+
   return (
     <div className="relative w-screen min-h-screen">
       <Routes>
